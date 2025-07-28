f3622c73e31cd01598a768139dfaad448ba70f59
Fernando Garrido <siberiancoffe@outlook.cl>
2025-07-28 10:20:44 -0400

    Implement initial FastAPI routes and templates; add styles for HTML responses

--- example_api.py
+++ example_api.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from db import selectDataPerritos as SeleccionarPerro
 from db import selectDataDuenos as SeleccionarDuenos
 
-
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World 4F"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"title": "Hola 4F"}
+    )
+
+@app.get("/listaperros", response_class=HTMLResponse)
+def read_root(request: Request):
+    db_perritos = SeleccionarPerro()
+    return templates.TemplateResponse(
+        request=request, name="listaperros.html", context={"perritos": db_perritos}
+    )
 
 @app.get("/perros/")
 def read_perros():
