22563dfc9714b0a0b1201c33373b0a2480c9541e
Fernando Garrido <siberiancoffe@outlook.cl>
2025-08-14 09:04:46 -0400

    Refactor example_api.py to update route handlers and integrate selectDataEjemplo; add formulario.html template for item creation

--- example_api.py
+++ example_api.py
@@ -3,7 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from db import selectDataPerritos as SeleccionarPerro
+from db import selectDataEjemplo
+from db import selectDataPerro as SeleccionarPerro
 from db import selectDataDuenos as SeleccionarDuenos
 
 from db import selectExampleData
@@ -18,17 +19,23 @@ def read_root(request: Request):
         request=request, name="index.html", context={"title": "Hola Mundo!!!"}
     )
 
-@app.get("/listaperros", response_class=HTMLResponse)
-def read_root(request: Request):
-    db_perritos = SeleccionarPerro()
+@app.get("/perros", response_class=HTMLResponse)
+def duhsdjkhfbsdkfb(request: Request):
+    db_ejemplo = selectDataEjemplo()
 
     contexto = {
-        "perritos": db_perritos,
+        "ejemplo": db_ejemplo,
         "title": "Lista Perritos 🦊"
     }
     
     return templates.TemplateResponse(
         request=request, name="listaperros.html", context=contexto
+    )
+
+@app.get("/perros/agregar")
+def otra_pagina_2 (request: Request):
+    return templates.TemplateResponse(
+        request=request, name="formulario.html"
     )
 
 @app.get("/perros/")
